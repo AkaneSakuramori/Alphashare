ea01296b05239b90eac0c4c5d9ad04df1bca45f5
AkaneSakuramori <utkarshffuff@gmail.com>
2025-08-24 23:43:21 +0530

    Create startmsg.py

---
+++ handlers/startmsg.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021-2025 @thealphabotz - All Rights Reserved.
+
+class Messages:
+    START_TEXT = """
+𝙷𝚎𝚕𝚕𝚘 {user_method}
+
+𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝚝𝚘 {bot_name}
+𝚂𝚎𝚗𝚍 𝚖𝚎 𝚏𝚒𝚕𝚎𝚜 𝚝𝚘 𝚐𝚎𝚝 𝚜𝚑𝚊𝚛𝚎𝚊𝚋𝚕𝚎 𝚕𝚒𝚗𝚔𝚜.
+"""
